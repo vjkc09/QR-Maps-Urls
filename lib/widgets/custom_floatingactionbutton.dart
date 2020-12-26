d2fba93748e52375d269282e4bfedebdd5683c4b
Victor Contreras <vjkc09@gmail.com>
2020-12-25 18:54:42 -0600

    feat: configuracion lector qr

--- lib/widgets/custom_floatingactionbutton.dart
+++ lib/widgets/custom_floatingactionbutton.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
 
 class CustomFloatingActionButton extends StatelessWidget {
   //const CustomFloatingActionButton({Key key}) : super(key: key);
@@ -10,7 +11,14 @@ class CustomFloatingActionButton extends StatelessWidget {
       child: Icon(
         Icons.filter_center_focus
       ),
-      onPressed: () {}
+      onPressed: () async {
+        String barcodeScanRes = await FlutterBarcodeScanner.scanBarcode(
+                                                    '#3D8BEF', 
+                                                    'Cancelar', 
+                                                    false, 
+                                                    ScanMode.QR);
+        print(barcodeScanRes);
+      }
       );
   }
 }
